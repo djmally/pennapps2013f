c86053f4e9e984ec566405a5420c9ff0bc547bce
nealchri <nealchri@sas.upenn.edu>
2013-09-07 13:03:48 -0400

    New version of project

---
+++ src/WristWalk/src/com/stwingApps/WristWalk/WristWalkActivity.java
@@ -0,0 +1,15 @@
+package com.stwingApps.WristWalk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WristWalkActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
