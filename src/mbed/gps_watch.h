9fb5ef58bed286049dd8589779e1abeff553fd20
David Mally <djmally@gmail.com>
2013-09-07 01:31:22 -0400

    Removed motor header and changed motors to DigitalOuts

--- src/mbed/gps_watch.h
+++ src/mbed/gps_watch.h
@@ -1,16 +1,15 @@
 #include "mbed.h"
-#include "Motor.h"
 
 char mode = 'g';
 
-Motor motor1(p21, p5, p6);
-Motor motor2(p22, p7, p8);
-Motor motor3(p23, p9, p10);
-Motor motor4(p24, p11, p12);
-Motor motor5(p25, p13, p14);
-Motor motor6(p26, p15, p16);
+DigitalOut motor0(p21);
+DigitalOut motor1(p22);
+DigitalOut motor2(p23);
+DigitalOut motor3(p24);
+DigitalOut motor4(p25);
+DigitalOut motor5(p26);
 
-Motor motor_arr[6] = {motor1, motor2, motor3, motor4, motor5, motor6}; 
+DigitalOut motor_arr[6] = {motor0, motor1, motor2, motor3, motor4, motor5};
 
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
