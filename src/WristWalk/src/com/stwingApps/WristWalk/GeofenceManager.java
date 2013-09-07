6de7ee448f4a3f1871d105a1eed725a0cc3ca85a
Richard Roberts <ricro@seas.upenn.edu>
2013-09-07 14:53:32 -0400

    Added Geofence Manager

---
+++ src/WristWalk/src/com/stwingApps/WristWalk/GeofenceManager.java
@@ -0,0 +1,18 @@
+package com.stwingApps.WristWalk;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.Geofence.Builder;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ricro
+ * Date: 9/7/13
+ * Time: 2:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GeofenceManager {
+
+    public GeofenceManager() {
+    }
+
+
+}
