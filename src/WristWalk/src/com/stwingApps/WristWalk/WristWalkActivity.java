b79b992bea0e10f73826c158d5d622e36e58faf2
David Mally <djmally@gmail.com>
2013-09-07 15:35:30 -0700

    Added bluetooth toggling

--- src/WristWalk/src/com/stwingApps/WristWalk/WristWalkActivity.java
+++ src/WristWalk/src/com/stwingApps/WristWalk/WristWalkActivity.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.bluetooth.*;
 import android.view.View;
+import com.google.android.gms.location.*;
 
 public class WristWalkActivity extends Activity {
+    private BluetoothAdapter bluetoothAdapter;
+
     /**
      * Called when the activity is first created.
      */
@@ -13,6 +16,7 @@ public class WristWalkActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     }
 
     @Override
@@ -25,5 +29,13 @@ public class WristWalkActivity extends Activity {
     public void getDirections(View view) {
         //This will call Google Maps for the direction data
 
+    }
+
+    public void toggleBluetooth(View view){
+        if(!bluetoothAdapter.isEnabled()){
+            bluetoothAdapter.enable();
+        } else{
+            bluetoothAdapter.disable();
+        }
     }
 }
